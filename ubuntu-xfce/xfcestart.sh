603e0a62c479cc9df9765b1a08b7e31b995df982
Ben Sandbrook <bsandbro@vt.edu>
2021-05-05 11:36:18 -0400

    add dbus support

--- ubuntu-xfce/xfcestart.sh
+++ ubuntu-xfce/xfcestart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export DISPLAY=:$(cat /tmp/$1/.Xdisplaynum | tr -d '\n')
-xfce4-session --disable-tcp &
+HOME="/tmp/$INSTANCE_ID/" dbus-launch --exit-with-session xfce4-session --disable-tcp &
 export XFCEPID=$!
 echo $XFCEPID > /tmp/$1/xfcepid
 wait $XFCEPID
