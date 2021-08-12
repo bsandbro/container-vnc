8784a4f84c719d7344130143914aef42b38d994c
Ben Sandbrook <bsandbro@vt.edu>
2021-08-12 17:03:08 -0400

    increase delay for display number check and export display for xfce

--- ubuntu-xfce/xfcestart.sh
+++ ubuntu-xfce/xfcestart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DISPLAY=:"$(tr -d '\n' < /TMPDIR/.Xdisplaynum)"
+export DISPLAY=:"$(tr -d '\n' < /TMPDIR/.Xdisplaynum)"
 dbus-launch --exit-with-session xfce4-session --disable-tcp &
 XFCEPID=$!
 echo $XFCEPID > /TMPDIR/xfcepid
